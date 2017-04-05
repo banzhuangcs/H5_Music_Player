931c5223616b437c8b52ba27e1c045ce826204e3
wjj <13524220689@189.cn>
2017-04-05 13:02:10 +0800

    LyricGroup歌词提示

--- src/components/content/Lyric/Lyric.jsx
+++ src/components/content/Lyric/Lyric.jsx
@@ -7,37 +7,29 @@ import React, { Component, PropTypes } from 'react';
 export default class Lyric extends Component {
   static propTypes = {
     text: PropTypes.string,
-    isFadeOut: PropTypes.bool,
     isHighLight: PropTypes.bool
   };
 
   static defaultProps = {
-    isFadeOut: false,
     isHighLight: false
   };
 
   constructor(props) {
     super(props);
 
-    this.getStyles = (isFadeOut, isHighLight) => ({
+    this.getStyles = (isHighLight) => ({
       display: '-webkit-box',
       WebkitBoxPack: 'center',
       marginBottom: 16,
-      transition: 'opacity .5s linear',
-      opacity: isFadeOut ? 0 : 1,
       color: isHighLight ? '#e03a3a' : '#666'
     });
   }
 
-  shouldComponentUpdate(nextProps) {
-    return nextProps.text !== this.props.text;
-  }
-
   render() {
-    const { text, isFadeOut, isHightLight } = this.props;
+    const { text, isHighLight } = this.props;
 
     return (
-      <div style={ this.getStyles(isFadeOut, isHightLight) }>{ text }</div>
+      <div style={ this.getStyles(isHighLight) }>{ text }</div>
     );
   }
 }
--- src/components/mainContent/LyricGroup/LyricGroup.jsx
+++ src/components/mainContent/LyricGroup/LyricGroup.jsx
@@ -3,6 +3,7 @@
 */
 
 import React, { Component, PropTypes } from 'react';
+import { findDOMNode } from 'react-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { getLyricInfo } from '../../../actionCreators/lyric';
@@ -13,6 +14,24 @@ class LyricGroup extends Component {
   static propTypes = {
     songlrc: PropTypes.string
   };
+
+  _calcTime(time) {
+    const [ minute, second ] = time.split(':');
+
+    return (+minute) * 60 + (+second);
+  }
+
+  _getPlayLyricIndex() {
+    const { lyrics, remainTime } = this.props;
+    const remainSecond = this._calcTime(remainTime);
+
+    return lyrics.findIndex(lyric => {
+      const lyricTime = lyric.remainTime.replace(/[\[\]]/g, '').replace(/\.\d+/, '');
+      const lyricSecond = this._calcTime(lyricTime);
+
+      return lyricSecond >= remainSecond;
+    });
+  }
 
   constructor(props) {
     super(props);
@@ -25,7 +44,7 @@ class LyricGroup extends Component {
         matchTime.lastIndex = 0;
 
         return {
-          useTime: matchTime.exec(text)[0].replace(/\[\]/g, ''),
+          remainTime: matchTime.exec(text)[0].replace(/\[\]/g, ''),
           part: text.replace(/\[.*?\]/, '').trim()
         };
       })
@@ -44,12 +63,17 @@ class LyricGroup extends Component {
 
     if (isVisible) {
       return (
-        <div className={ style['lyric-group'] }>
+        <div
+          className={ style['lyric-group'] }
+          ref={ el => this.lyricGroupEl = el }>
           <div className={ style['lyric-group-inner'] }>
             <div className={ style['lyric-group-panel'] }>
               { lyrics.map((lyric, index) =>
-                <Lyric key={ index } text={ lyric.part } />
-              ) }
+                  <Lyric
+                    ref={ `lyric${ index }` }
+                    key={ index }
+                    isHighLight={ this._getPlayLyricIndex() === index }
+                    text={ lyric.part } />) }
             </div>
           </div>
         </div>
@@ -58,12 +82,44 @@ class LyricGroup extends Component {
 
     return null;
   }
+
+  componentDidUpdate() {
+
+    //this.lyricGroupEl.scrollTop = 100;
+    // const singleLyricEl = findDOMNode(this.refs['lyric0']);
+    //
+    // if (singleLyricEl) {
+    //   const scrollDistance =
+    //     singleLyricEl.offsetHeight + parseInt(singleLyricEl.style.marginBottom);
+    //
+    //   Object.keys(this.refs).forEach(name => {
+    //     const com = this.refs[name];
+    //     const el = findDOMNode(com);
+    //
+    //     if (com.props.isHighLight
+    //       && el.offsetTop > this.lyricGroupEl.offsetHeight
+    //       && this.shouldScrollBottom) {
+    //       this.lyricGroupEl.height = 1000 + 'px';
+    //       //this.lyricGroupEl.scrollTop += `${ this.lyricGroupEl.scrollTop + scrollDistance }px`;
+    //       //this.lyricGroupEl.scrollTo(0, this.lyricGroupEl.scrollTop + scrollDistance);
+    //     }
+    //   });
+    // }
+  }
+
 }
 
 export default connect(
   ({
     lyric: {
       lyrics = [],
-      isVisible = true } }) => ({ lyrics, isVisible }),
+      isVisible = true,
+      remainTime = '00:00'
+    }
+  }) => ({
+    lyrics,
+    isVisible,
+    remainTime
+  }),
   (dispatch) => bindActionCreators({ getLyricInfo }, dispatch)
 )(LyricGroup);
