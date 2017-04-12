8ef1ad6adca76c7862e42fbfed21e108e2951523
wjj <13524220689@189.cn>
2017-04-12 13:35:10 +0800

    initial commit

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-/**
- * store
-*/
-
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducer from '../reducers/reducer';
-
-export default createStore(reducer, applyMiddleware(thunk));
